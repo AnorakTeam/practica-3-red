1988bbac18632a509d91022e47fff1c143c4b20b
José Manuel Pérez Rodríguez <anorakteam@gmail.com>
2026-09-20 13:27:50 -0500

    feat: agregar arranque y configuración de máquina con outputs

--- variables.tf
+++ variables.tf
@@ -2,6 +2,15 @@
 
 variable "proyecto"     { type = string }
 variable "prefijo"      { type = string }
+variable "tipo_maquina" { 
+    type = string
+    default = "e2-micro"
+}
+
+variable "zona" {
+    type = string
+    default = "us-central1-a"
+}
 
 variable "region"       { 
     type = string  
